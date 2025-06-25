2da339bbd267ddeb1359498fc95e9da4850de0e1
Robert Black <allen@allenblack.org>
2025-06-24 20:24:13 -0400

    add a postgres status update line in the home route

--- app.py
+++ app.py
@@ -6,13 +6,14 @@ app = Flask(__name__)
 
 
 @app.route('/')
-def hello_world():  # put application's code here
+def home():
     return (
         "Welcome to my Flask BI project!<br><br>"
         "This is a personal, AI-assisted learning app built to explore Python, Flask, and cloud deployment.<br><br>"
         "The goals of this project include:<br>"
         "- Building interactive web applications using Flask<br>"
         "- Connecting to and querying a PostgreSQL database<br>"
+        "&nbsp;&nbsp;&nbsp;&nbsp;&bull; Demonstration route: <a href='/pocdb'>/pocdb</a> (or visit allenblack.org/pocdb)<br>"
         "- Transforming and presenting data using Python<br>"
         "- Deploying apps to the cloud via Render<br><br>"
         "Check back as new features and routes are added over time."
