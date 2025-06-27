396fc9e9f2c6a12d355c4e2a56078503c25f36c8
Robert Black <allen@allenblack.org>
2025-06-27 18:45:52 -0400

    add a postgres status update line in the home route

--- app.py
+++ app.py
@@ -13,11 +13,33 @@ def home():
         "The goals of this project include:<br>"
         "- Building interactive web applications using Flask<br>"
         "- Connecting to and querying a PostgreSQL database<br>"
-        "&nbsp;&nbsp;&nbsp;&nbsp;&bull; Demonstration route: <a href='/pocdb'>/pocdb</a> (or visit allenblack.org/pocdb)<br>"
+        "&nbsp;&nbsp;&nbsp;&nbsp;&bull; Demonstration route: <a href='/dbtest'>/dbtest</a> (or visit allenblack.org/dbtest)<br>"
         "- Transforming and presenting data using Python<br>"
         "- Deploying apps to the cloud via Render<br><br>"
         "Check back as new features and routes are added over time."
     )
+#db connection for local docker postgres
+def get_db_connection():
+    conn = psycopg2.connect(
+        host="dpg-d1djjc3ipnbc73dbvi6g-a.virginia-postgres.render.com",
+        database="pocdb_te73",
+        user="pocdb_te73_user",
+        password="TnlIBboc9mOqMrXWiYd1hiseRWpiCbPN"
+    )
+    return conn
+
+#route for local docker postgres
+@app.route("/dbtest")
+def test_db():
+    conn = get_db_connection()
+    cur = conn.cursor()
+    cur.execute("SELECT * FROM dist_perf_dw.dim_expense_types limit 10;")
+    rows = cur.fetchall()
+    cur.close()
+    conn.close()
+
+    formatted = [f"{r[0]} {r[1]}  {r[2]} {r[3]}" for r in rows]
+    return "<br>".join(formatted)
 
 # Additional route that returns structured JSON
 @app.route('/pocdb')
