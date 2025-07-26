b1f652794355862966d6f908bdaf85d43df3b575
Robert Black <allen@allenblack.org>
2025-07-26 17:40:52 -0400

    setup dummy sql staging files for debug

--- dbt/projects/dist_perf_analytics/models/staging/stg_dim_accounts.sql
+++ dbt/projects/dist_perf_analytics/models/staging/stg_dim_accounts.sql
@@ -1,3 +1,4 @@
 -- stg_dim_accounts.sql
-{{ config(materialized='view') }}
-SELECT id, account_name, account_code, base_fee_rate, created_at, updated_at FROM {{ source('raw', 'dim_accounts') }}
+--{{ config(materialized='view') }}
+--SELECT id, account_name, account_code, base_fee_rate, created_at, updated_at FROM {{ source('raw', 'dim_accounts') }}
+select 1 as test_column
